81387ec2787bbf105b1a269515605cf4927d581b
user <nobody>
2015-04-25 21:05:50 -0700

    Specify number of tracks when writing the file header
    If you can specify how many tracks, instead of using
    len(pattern), you could stream tracks to the destination
    (timidity or whatever).
    
    There's little point in doing this for tracks, since their
    length has to be the BYTE count of all their events. If
    you can count how many bytes a collection of MIDI events is
    going to be, without accumulating them into a byte buffer,
    you're a better soul than I.

--- examples/timidity.py
+++ examples/timidity.py
@@ -3,16 +3,24 @@ import midi
 import subprocess as s
 
 def main():
-    p = s.Popen(["timidity","-"],stdin=s.PIPE)
+    p = s.Popen(["timidity"]+['--verbose']*1+["-"],stdin=s.PIPE)
     writer = midi.FileWriter(p.stdin)
-    writer.write_file_header(midi.Pattern())
-    writer.write_track_header(1)
+    writer.write_file_header(midi.Pattern(),2)
+    track = midi.Track((
+        midi.TimeSignatureEvent(tick=0,numerator=4,denominator=4,
+                            metronome=24,
+                            thirtyseconds=8),
+        midi.KeySignatureEvent(tick=0),
+        midi.EndOfTrackEvent(tick=1)
+    ))
+    writer.write_track(track)
 
     def song():
-        for i in range(200):
-            yield midi.NoteOnEvent(tick=i*100, velocity=200, pitch=midi.G_3)
-    for event in song():
-        writer.write_midi_event(event)
+        for i in range(5):
+            yield midi.NoteOnEvent(tick=i*100, velocity=200, pitch=(midi.G_5+i))
+        yield midi.EndOfTrackEvent(tick=1)
+    writer.write_track(midi.Track(song()))
+    p.stdin.flush()
     p.stdin.close()
     p.wait()
     

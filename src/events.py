5db02c195f7bea156954f54cb343ce8b05f0fb07
Marius Gedminas <marius@gedmin.as>
2017-10-10 20:37:30 +0300

    Include own copy of with_metaclass()

--- src/events.py
+++ src/events.py
@@ -1,5 +1,4 @@
 import math
-from future.utils import with_metaclass
 
 class EventRegistry(object):
     Events = {}
@@ -25,6 +24,23 @@ class RegisterEventMeta(type):
         if name not in ['AbstractEvent', 'Event', 'MetaEvent', 'NoteEvent',
                         'MetaEventWithText']:
             EventRegistry.register_event(cls, bases)
+
+
+def with_metaclass(meta, *bases):
+    """Create a base class with a metaclass."""
+    # This requires a bit of explanation: the basic idea is to make a dummy
+    # metaclass for one level of class instantiation that replaces itself with
+    # the actual metaclass.
+    class metaclass(type):
+
+        def __new__(cls, name, this_bases, d):
+            return meta(name, bases, d)
+
+        @classmethod
+        def __prepare__(cls, name, this_bases):
+            return meta.__prepare__(name, bases)
+    return type.__new__(metaclass, 'temporary_class', (), {})
+
 
 class AbstractEvent(with_metaclass(RegisterEventMeta,object)):
     __slots__ = ['tick', 'data']
